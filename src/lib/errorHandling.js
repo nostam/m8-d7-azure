d5ee99dd4060f83d16704f2b986afe699e5ff172
nostam <matsonho@gmail.com>
2021-01-13 15:39:59 +0100

    update error handler with mongodb

--- src/lib/errorHandling.js
+++ src/lib/errorHandling.js
@@ -1,26 +1,26 @@
 const badRequestHandler = (err, req, res, next) => {
-  if (err.httpStatusCode === 400) {
+  if (err.httpStatusCode === 400 || err.statusCode === 400) {
     res.status(400).send("Bad Request");
   }
   next(err);
 };
 
 const notFoundHandler = (err, req, res, next) => {
-  if (err.httpStatusCode === 404) {
+  if (err.httpStatusCode === 404 || err.statusCode === 404) {
     res.status(404).send("Not found");
   }
   next(err);
 };
 
 const unauthorizedHandler = (err, req, res, next) => {
-  if (err.httpStatusCode === 401) {
+  if (err.httpStatusCode === 401 || err.statusCode === 401) {
     res.status(401).send("Unauthorized");
   }
   next(err);
 };
 
 const forbiddenHandler = (err, req, res, next) => {
-  if (err.httpStatusCode === 403) {
+  if (err.httpStatusCode === 403 || err.statusCode === 403) {
     res.status(403).send("Forbidden");
   }
   next(err);
@@ -28,6 +28,7 @@ const forbiddenHandler = (err, req, res, next) => {
 
 const catchAllHandler = (err, req, res, next) => {
   if (!res.headersSent) {
+    console.log(err);
     res.status(err.httpStatusCode || 500).send("Generic Server Error");
   }
 };
--- src/services/articles/index.js
+++ src/services/articles/index.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const articlesRouter = express.Router();
-const ArticlesModel = require("./articlesModel");
-const { body, validationResult } = require("express-validator");
+const ArticlesModel = require("../../model/articles");
+const ReviewsModel = require("../../model/reviews");
+const { param, body, validationResult } = require("express-validator");
 const { err } = require("../../lib");
 
-const validateReq = [
-  body("headline").notEmpty().isString(),
+const validateArticle = [
+  body("headLine").notEmpty().isString(),
   body("subHead").isString(),
   body("content").notEmpty().isString(),
-  body("category.*.name").isString(),
-  body("category.*.img").isURL(),
-  body("author.*.name").isString(),
-  body("author.*.img").isURL(),
-  body("cover").isURL,
+  body("category.name").isString(),
+  body("category.img").isURL(),
+  body("author.name").isString(),
+  body("author.img").isURL(),
+  body("cover").isURL(),
+];
+
+const validateReview = [
+  body("text").isString(),
+  body("user").notEmpty().isString(),
+  // param("id").isMongoId(),
 ];
 
 articlesRouter.get("/", async (req, res, next) => {
@@ -27,19 +34,39 @@ articlesRouter.get("/", async (req, res, next) => {
 articlesRouter.get("/:id", async (req, res, next) => {
   try {
     const article = await ArticlesModel.findById(req.params.id);
-    if (!article) return next(err(`${req.params.id} not found`, 404));
+    // if (!article) return next(err(`${req.params.id} not found`, 404));
     res.send(article);
   } catch (error) {
     next(error);
   }
 });
 
-articlesRouter.post("/", validateReq, async (req, res, next) => {
+articlesRouter.get("/:id/reviews", async (req, res, next) => {
+  try {
+    const reviews = await ReviewsModel.find({ articleId: req.params.id });
+    res.send(reviews);
+  } catch (error) {
+    next(error);
+  }
+});
+
+articlesRouter.get("/:id/reviews/:reviewID", async (req, res, next) => {
+  try {
+    const review = await ReviewsModel.findById(req.params.reviewId);
+    res.send(review);
+  } catch (error) {
+    next(error);
+  }
+});
+
+articlesRouter.post("/", validateArticle, async (req, res, next) => {
   try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) return next(err(errors.array(), 400));
-
-    const newPost = new ArticlesModel(req.body);
+    const newPost = new ArticlesModel(req.body, {
+      runValidators: true,
+      new: true,
+    });
     const { _id } = await newPost.save();
     res.status(201).send(_id);
   } catch (error) {
@@ -47,7 +74,22 @@ articlesRouter.post("/", validateReq, async (req, res, next) => {
   }
 });
 
-articlesRouter.put("/:id", validateReq, async (req, res, next) => {
+articlesRouter.post("/:id", validateReview, async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return next(err(errors.array(), 400));
+    const newReview = new ReviewsModel({
+      ...req.body,
+      articleID: req.params.id,
+    });
+    const { _id } = await newReview.save();
+    res.status(201).send(_id);
+  } catch (error) {
+    next(error);
+  }
+});
+
+articlesRouter.put("/:id", validateArticle, async (req, res, next) => {
   try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) return next(err(errors.array(), 400));
@@ -63,11 +105,29 @@ articlesRouter.put("/:id", validateReq, async (req, res, next) => {
     next(error);
   }
 });
-
+articlesRouter.put(
+  "/:id/reviews/:reviewID",
+  validateReview,
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) return next(err(errors.array(), 400));
+      console.log(await ReviewsModel.findById(req.params.reviewID));
+      const review = await ReviewsModel.findByIdAndUpdate(
+        req.params.reviewID,
+        req.body,
+        { runValidators: true, new: true }
+      );
+      res.send(review);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 articlesRouter.delete("/:id", async (req, res, next) => {
   try {
     const articles = await ArticlesModel.findByIdAndDelete(req.params.id);
-    if (!articles) return next(err(`${req.params.id} not found`, 404));
+    // if (!articles) return next(err(`${req.params.id} not found`, 404));
     res.status(204).send();
   } catch (error) {
     next(error);
